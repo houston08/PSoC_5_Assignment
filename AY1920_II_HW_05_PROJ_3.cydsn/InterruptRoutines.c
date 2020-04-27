dc75f48b894c7507ab2e82600d3eb977b87f2e96
Michele <michele.grimaldi128@gmail.com>
2020-04-28 00:17:32 +0200

    Project 3 Started and 2.3.1 point done: correctly set the control register to output a 3 Axis Signal in High Resolution Mode at 100 Hz in the ±4.0 g FSR

---
+++ AY1920_II_HW_05_PROJ_3.cydsn/InterruptRoutines.c
@@ -0,0 +1,27 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "project.h"
+#include "InterruptRoutines.h"
+/* Since we want to sample the sensor output at a frequency 
+   of 100Hz, the timer is set to generate an Interrupt service
+   routine every 10ms. The isr will change the value of volatile
+   flag_ready0 variable to 1 leading to the reading of the sensor
+   output in main function.     */
+
+_VOLATILE  uint8 flag_ready0; // Volatile Global variable enabling to sample
+
+CY_ISR(Custom_TIMER_ISR)
+{
+    Timer_ReadStatusRegister();
+    flag_ready0=1;
+}
+/* [] END OF FILE */
