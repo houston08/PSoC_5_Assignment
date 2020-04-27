ab01710e7c3207ff16494f83570c96a204a74897
Michele <michele.grimaldi128@gmail.com>
2020-04-27 17:38:13 +0200

    Renamed iic and ini file for Bridge control panel + comments on InterruptRoutines.c

--- AY1920_II_HW_05_PROJ_2.cydsn/InterruptRoutines.c
+++ AY1920_II_HW_05_PROJ_2.cydsn/InterruptRoutines.c
@@ -15,8 +15,9 @@
    of 100Hz, the timer is set to generate an Interrupt service
    routine every 10ms. The isr will change the value of volatile
    flag_ready0 variable to 1 leading to the reading of the sensor
-   output in main function.                                     */
-_VOLATILE  uint8 flag_ready0; // Global variable 
+   output in main function.     */
+
+_VOLATILE  uint8 flag_ready0; // Volatile Global variable enabling to sample
 
 CY_ISR(Custom_TIMER_ISR)
 {
